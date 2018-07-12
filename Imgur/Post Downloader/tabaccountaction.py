3302adc4d17f4e436ed658e5d2a5bcd7ebf1c55f
acomputerguy <copyandpaste0@gmail.com>
2018-07-12 01:55:49 -0400

    Create tabaccountaction.py

---
+++ Imgur/Post Downloader/tabaccountaction.py
@@ -0,0 +1,13 @@
+class SubmitButton():
+	def CollectCreds(self, clientID, sharedSecret):
+		if(clientID == ""):
+			print("empty")
+		else:
+			print("not empty")
+		if(sharedSecret == ""):
+			print("empty")
+		else:
+			print("not empty")
+			print("write to yaml file")
+		#print(self.ui.clientIDLineEdit.text())
+		#print(self.ui.clientSecretLineEdit.text())
